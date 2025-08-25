f9dc94b5f78fc602b19fa57ecbea4ac82394a190
joaocarlos8 <joaocarlostrapasson88@gmail.com>
2025-08-25 19:24:12 -0300

    SKINS DO CSGO

--- GranStore/Controllers/HomeController.cs
+++ GranStore/Controllers/HomeController.cs
@@ -15,96 +15,97 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        List<Categoria> categorias = new() {
-            new Categoria { Id = 1, Nome = "Mouse"},
-            new Categoria { Id = 2, Nome = "Mousepad"}
+        List<Categoria> categorias = new()
+        {
+            new Categoria { Id = 1, Nome = "Skins" }
         };
 
         List<Produto> produtos = new List<Produto>
         {
             new Produto {
-            Id = 1,
-            Categoria = categorias[0],
-            Nome = "Pulsar ZywOo",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 1649.90m,
-            ValorVenda = 1484.90m,
-            QtdeEstoque = 26,
-            Foto = "/img/Produtos/pulsar-zywoo-medium-white.jpg",
-        },
+                Id = 1,
+                Categoria = categorias[0],
+                Nome = "AWP | Dragon Lore (Factory New)",
+                Descricao = "A lendária skin da AWP com status FN, extremamente rara.",
+                ValorCusto = 150000.00m,
+                ValorVenda = 135000.00m,
+                QtdeEstoque = 2,
+                Foto = "/img/Skins/awp-dragon-lore-fn.jpg"
+            },
             new Produto {
-            Id = 2,
-            Categoria = categorias[0],
-            Nome = "Finalmouse ULX Prophecy Small",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 2199.90m,
-            ValorVenda = 1979.91m,
-            QtdeEstoque = 43,
-            Foto = "/img/Produtos/Finalmouse ULX Prophecy Small.jpg"
-        },
+                Id = 2,
+                Categoria = categorias[0],
+                Nome = "Karambit | Case Hardened (Blue Gem)",
+                Descricao = "Karambit com pattern blue gem raro.",
+                ValorCusto = 180000.00m,
+                ValorVenda = 165000.00m,
+                QtdeEstoque = 1,
+                Foto = "/img/Skins/karambit-case-hardened-bluegem.jpg"
+            },
             new Produto {
-            Id = 3,
-            Categoria = categorias[0],
-            Nome = "Viper V3 PRO",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 1899.90m,
-            ValorVenda = 1709.91m,
-            QtdeEstoque = 29,
-            Foto = "/img/Produtos/ViperV3PRO.jpg"
-        },
+                Id = 3,
+                Categoria = categorias[0],
+                Nome = "AK-47 | Wild Lotus (Factory New)",
+                Descricao = "Skin floral rara e desejada da AK-47.",
+                ValorCusto = 60000.00m,
+                ValorVenda = 54000.00m,
+                QtdeEstoque = 3,
+                Foto = "/img/Skins/ak47-wild-lotus.jpg"
+            },
             new Produto {
-            Id = 4,
-            Categoria = categorias[0],
-            Nome = "Endgame Gear OP1w 4K v2",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 1099.90m,
-            ValorVenda = 989.91m,
-            QtdeEstoque = 14,
-            Foto = "/img/Produtos/Endgame Gear OP1w 4K v2.jpg"
-        },
-          new Produto {
-            Id = 5,
-            Categoria = categorias[0],
-            Nome = "KanamiMinamiGlasspad",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 1499.90m,
-            ValorVenda = 1349.91m,
-            QtdeEstoque = 21,
-            Foto = "/img/Produtos/KanamiMinamiGlasspad.jpg"
-        },
-          new Produto {
-            Id = 6,
-            Categoria = categorias[0],
-            Nome = "Wallhack SP-004",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 1299.90m,
-            ValorVenda = 1079.91m,
-            QtdeEstoque = 23,
-            Foto = "/img/Produtos/Wallhack SP-004.jpg"
-        },
-          new Produto {
-            Id = 7,
-            Categoria = categorias[0],
-            Nome = "LGG x Pulsar Hyperion XXL",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 1199.90m,
-            ValorVenda = 944.91m,
-            QtdeEstoque = 22,
-            Foto = "/img/Produtos/LGGxPulsarHyperionXXL.jpg"
-        },
-          new Produto {
-            Id = 8,
-            Categoria = categorias[0],
-            Nome = "Artisan FX Key-83 XL",
-            Descricao = "Lorem Ipsum",
-            ValorCusto = 749.90m,
-            ValorVenda = 674.91m,
-            QtdeEstoque = 9,
-            Foto = "/img/Produtos/ArtisanFXKey-83XL.jpg"
-        },
+                Id = 4,
+                Categoria = categorias[0],
+                Nome = "M4A4 | Howl (Factory New)",
+                Descricao = "Skin contrabandeada e exclusiva da M4A4.",
+                ValorCusto = 45000.00m,
+                ValorVenda = 41000.00m,
+                QtdeEstoque = 2,
+                Foto = "/img/Skins/m4a4-howl.jpg"
+            },
+            new Produto {
+                Id = 5,
+                Categoria = categorias[0],
+                Nome = "Butterfly Knife | Crimson Web (Factory New)",
+                Descricao = "Faca extremamente rara com web pattern ideal.",
+                ValorCusto = 90000.00m,
+                ValorVenda = 84999.00m,
+                QtdeEstoque = 1,
+                Foto = "/img/Skins/butterfly-crimson-web.jpg"
+            },
+            new Produto {
+                Id = 6,
+                Categoria = categorias[0],
+                Nome = "Souvenir AWP | Medusa (FN)",
+                Descricao = "Skin souvenir da AWP Medusa com adesivos de torneios.",
+                ValorCusto = 120000.00m,
+                ValorVenda = 112000.00m,
+                QtdeEstoque = 1,
+                Foto = "/img/Skins/awp-medusa-souvenir.jpg"
+            },
+            new Produto {
+                Id = 7,
+                Categoria = categorias[0],
+                Nome = "StatTrak™ M9 Bayonet | Doppler (Sapphire)",
+                Descricao = "Faca Doppler Sapphire com StatTrak™.",
+                ValorCusto = 85000.00m,
+                ValorVenda = 79999.00m,
+                QtdeEstoque = 2,
+                Foto = "/img/Skins/m9-doppler-sapphire.jpg"
+            },
+            new Produto {
+                Id = 8,
+                Categoria = categorias[0],
+                Nome = "Desert Eagle | Blaze (FN)",
+                Descricao = "Skin clássica e popular da Deagle.",
+                ValorCusto = 20000.00m,
+                ValorVenda = 18999.00m,
+                QtdeEstoque = 5,
+                Foto = "/img/Skins/deagle-blaze.jpg"
+            }
+        };
 
-    };
-
+        // Aqui você pode passar os produtos para a view, se desejar.
+        // Exemplo: return View(produtos);
         return View();
     }
 
